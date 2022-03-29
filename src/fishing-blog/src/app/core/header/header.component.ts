2c975ade0ff4667273eccbcaa3afdb884f50cbf1
Dimo Yanev <dyanev@kpmg.com>
2022-03-30 01:54:02 +0300

    add favicon and title

--- src/fishing-blog/src/app/core/header/header.component.ts
+++ src/fishing-blog/src/app/core/header/header.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent  {
 
   isLogged: boolean = false;
   isExpanded: boolean = false;
 
+
 toggle(): void {
     this.isExpanded = !this.isExpanded;
-    console.log(this.isExpanded)
   }
 
   logout(): void {
