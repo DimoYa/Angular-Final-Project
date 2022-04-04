67a517a44686499bc1e007d88cfc77f47003517e
Dimo Yanev <Dimo_Yanev@abv.bg>
2022-04-05 01:53:12 +0300

    create and apply guards

--- src/fishing-blog/src/app/authentication/authentication-routing.module.ts
+++ src/fishing-blog/src/app/authentication/authentication-routing.module.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { ProfileComponent } from "./profile/profile.component";
 import { ProfileUpdateComponent } from "./profile-update/profile-update.component";
+import { UserGuard } from "../core/guard/user.guard";
+import { AuthenticatedGuard } from "../core/guard/authenticated.guard";
 
 const routes: Routes = [
-    { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'profile/edit/:id', component: ProfileUpdateComponent },
+    { path: 'register',canActivate: [AuthenticatedGuard], component: RegisterComponent },
+    { path: 'login',canActivate: [AuthenticatedGuard], component: LoginComponent },
+    { path: 'profile',canActivate: [UserGuard], component: ProfileComponent },
+    { path: 'profile/edit/:id',canActivate: [UserGuard], component: ProfileUpdateComponent },
   ];
   
   export const AuthenticationRoutingModule = RouterModule.forChild(routes);
